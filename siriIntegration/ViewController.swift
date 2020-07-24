e3bb57ee328485986e5fe06b2180fd41cd9faa3b
jawaheralaggl <jawaheralaggl@gmail.com>
2020-07-24 12:09:10 +0300

    first commit

--- siriIntegration/ViewController.swift
+++ siriIntegration/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBOutlet weak var billLabel: UILabel!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func addButton(_ sender: Any) {
+        addBill()
+    }
+    
+    func addBill() {
+        let randomNumber = Int.random(in: 1...100)
+        label.text = "\(randomNumber)"
+        
+        createUserActivity()
+        
+        print("You Bill Is: \(randomNumber)")
+    }
+    
+    func createUserActivity() {
+        
+        let activity = NSUserActivity(activityType: "com.jaDanRA.siriIntegration.addBill")
+        
+        activity.title = "Add New Bill"
+        activity.isEligibleForSearch = true
+        activity.isEligibleForPrediction = true
+        
+        self.userActivity = activity
+        self.userActivity?.becomeCurrent()
+        
+    }
+    
+    
+    
 }
+
+
+
 
